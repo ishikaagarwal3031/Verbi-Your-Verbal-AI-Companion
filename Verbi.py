c9728c27a37ab4880790b6d6cb23534163f73506
Ishika Agarwal <114424310+ishikaagarwal3031@users.noreply.github.com>
2024-07-19 20:18:10 +0530

    Add files via upload

---
+++ Verbi.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('Hey, please speak..I am listening to you...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'verbi' in command:
+                command = command.replace('verbi', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_verbi():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'date' in command:
+        date = datetime.datetime.now().strftime('%B %d, %Y')
+        talk('Today\'s date is ' + date)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+while True:
+    run_verbi()
